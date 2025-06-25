18015864ce31428da0dab34a70647c52b31dcd1e
Rohitkumar367 <rohitddroid@gmail.com>
2025-06-25 23:45:13 +0530

    started finish form

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from 'react'
-import './App.css'
-import { NavBar } from './components/index.js'
+import './App.css';
+import { NavBar } from './components/index.js';
+import Finish from "./forms/Finish.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <NavBar/>
-    </>
-  )
+    <Router>
+      <NavBar />
+
+      <Routes>
+        <Route path="/finish" element={<Finish />} />
+      </Routes>
+
+    </Router>
+  );
 }
 
-export default App
+export default App;
