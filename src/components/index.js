50f007158d4914d607e322dc97ff41ca932d069f
amit chowdhury <camit8546@gmail.com>
2025-07-10 01:55:08 +0530

    changes in index.js of component folder

--- src/components/index.js
+++ src/components/index.js
@@ -1,18 +1,6 @@
 import NavBar from "./NavBar";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import Footer from "./Footer";
 export{
     NavBar,
     Footer
-=======
-export{
-    NavBar
->>>>>>> origin/development
-=======
-import Footer from "./Footer";
-export{
-    NavBar,
-    Footer
->>>>>>> 22635cd18810b6444fb84a6f04ca07da39ad9c1d
 }
