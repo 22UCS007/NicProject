3ad6c12ad6602f40312be320b30ef9660da89222
Rohitkumar367 <rohitddroid@gmail.com>
2025-07-15 20:36:26 +0530

    merged the updated

--- src/forms/Finish.jsx
+++ src/forms/Finish.jsx
@@ -1,13 +1,14 @@
 import React from "react"
 import { useState } from "react"
 import FormApprover from "../components/finish/FormApprover"
-const Finish = () =>{
+import FormChecker from "../components/finish/FormChecker"
+const Finish = ({userRole, userData}) =>{
   return (
     <div className="flex w-full">
       {/* <LeftMenu activeItem="Finish" /> */}
 
       <div className="w-full">
-        <FormApprover/>
+        {userRole==="approver" ? <FormApprover/> : <FormChecker/>}
       </div>
     </div>
   )
